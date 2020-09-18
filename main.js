93cbc061718822021bc88236ac65d388a64a8810
Ademuyiwa Adewuyi <ademuyiwaadewuyi@gmail.com>
2020-09-18 01:59:27 +0100

    added a ScrollReveal() library

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal("main .wrapper", {
+  origin: "left",
+  delay: 500,
+  reset: "true",
+  duration: 600,
+  distance: "50px"
+});
+
+sr.reveal(".main-nav", {
+  origin: "top",
+  duration: 600,
+  distance: "50em",
+  scale: 0.3,
+  delay: 500
+});
+
 
 /* const deleterList = document.querySelectorAll("span:nth-of-type(2)");
 deleterList.forEach(function(deleter) {
@@ -15,3 +34,20 @@ const bookList = document.querySelector("#booklist");
       bookList.removeChild(li);
     }
   });
+
+  /* const btn = document.querySelector("button");
+  btn.addEventListener("submit", function(e) {
+    const span = document.createElement("span");
+    span.textContent = document.querySelector("#added-books #add-book").value;
+    span.setAttribute("class", "deleter");
+    const li = document.createElement("li");
+    li.appendChild(span);
+    bookList.appendChild(li);
+  }); */
+
+const addBooks = document.forms["added-books"];
+addBooks.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const value = addBooks.querySelector("input[type='text']").value;
+  console.log(value);
+});
